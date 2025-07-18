9ea39e1dfc725cd5d6b776a37b0c13476f2c28d2
mahabub-bd <palashmahabub@gmail.com>
2025-07-18 21:18:34 +0600

    some fix

--- components/footer/copyright.tsx
+++ components/footer/copyright.tsx
@@ -11,16 +11,9 @@ const Copyright = () => {
         {/* Main Content Section */}
         <div className="py-8 md:py-12">
           <div className="grid grid-cols-1 md:grid-cols-12 gap-6 md:gap-12  items-center h-full">
-            {/* Copyright Text - 25% width */}
-            <div className="col-span-1 md:col-span-3 text-center md:text-left flex justify-center items-center">
-              <p className="text-[#deb149] text-sm  font-medium">
-                © {currentYear} German Butcher All rights reserved.
-              </p>
-            </div>
-
             {/* Payment Methods - 50% width */}
-            <div className="col-span-1 md:col-span-6 flex flex-col items-center space-y-4">
-              <h3 className="font-semibold text-lg text-white">
+            <div className="col-span-1 md:col-span-8 flex flex-col items-center space-y-4">
+              <h3 className="font-semibold text-2xl gradient-text ">
                 Online Payment
               </h3>
               <div className="w-full ">
@@ -34,7 +27,7 @@ const Copyright = () => {
 
             {/* Legal Links - 25% width */}
 
-            <div className="col-span-1 md:col-span-3 flex  md:flex-row items-center justify-center md:gap-4 gap-4 mx-auto">
+            <div className="col-span-1 md:col-span-4 flex  md:flex-row items-center justify-center md:gap-4 gap-4 mx-auto">
               <Link
                 className="px-4 md:px-4 py-2 md:py-2.5 text-xs  text-gray-300 border border-[#deb149] rounded-full backdrop-blur-sm transition-all duration-300 hover:bg-[#deb149] hover:text-black hover:border-[#deb149] hover:scale-105 hover:shadow-lg hover:shadow-[#deb149]/20 group overflow-hidden "
                 href="/privacy-policy"
@@ -53,7 +46,12 @@ const Copyright = () => {
 
         {/* Bottom Bar */}
         <div className="border-t border-gray-700 py-4 mx-auto">
-          <div className="flex flex-col md:flex-row items-center justify-between text-center md:text-left">
+          <div className="flex flex-col md:flex-row items-center justify-between text-center md:text-left gap-5">
+            <div className="col-span-1 md:col-span-3 text-center gradient-text md:text-left flex justify-center items-center">
+              <p className="  text-sm  font-medium">
+                © {currentYear} German Butcher All rights reserved.
+              </p>
+            </div>
             <Link
               href="https://www.techqul.com"
               className="text-gray-400 text-xs md:text-sm"
