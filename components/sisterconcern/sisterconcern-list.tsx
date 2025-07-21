d59163bf4441dda2099fa5e334bcc2b2745427c4
Mahabub Hossain <palashmahabub@gmail.com>
2025-07-21 16:03:20 +0600

    add our brand page fix

--- components/sisterconcern/sisterconcern-list.tsx
+++ components/sisterconcern/sisterconcern-list.tsx
@@ -4,7 +4,7 @@ import SisterConcernCard from "./sisterconcern-card";
 
 const SisterConcernsList: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 md:py-10 py-5 md:px-4 px-4 mt-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:py-10 py-5 md:px-4 px-4 mt-10">
       {sisterConcerns.map((concern: SisterConcern) => (
         <SisterConcernCard key={concern.id} concern={concern} />
       ))}
